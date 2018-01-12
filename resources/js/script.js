04fe278c215245850805bb6c265202bc24fe096c
gomisha <misha@gomisha.com>
2018-01-12 13:59:02 -0500

    added sticky scrolling to nav menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-   $('h1').click(function() {
-       $(this).css('background-color', '#ff0000');
-   })
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        }
+        else {
+            $('nav').removeClass('sticky');
+        }
+    });
+   //$('h1').click(function() {
+//       $(this).css('background-color', '#ff0000');
+//   })
+    
+    /*
+    
+var waypoint = new Waypoint({
+  element: document.getElementById('element-waypoint'),
+  handler: function(direction) {
+    notify(this.element.id + ' triggers at ' + this.triggerPoint)
+  },
+  offset: '75%'
+})    
+    */
 });
