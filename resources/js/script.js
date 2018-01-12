57303043deeaa12c846b606d74e2b366ec6ccfc7
gomisha <misha@gomisha.com>
2018-01-12 15:55:48 -0500

    added jquery / css animations on scroll (with animate.css)

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,4 +60,32 @@ $(document).ready(function() {
           }
         }
       });
+    
+    
+    //ANIMATION ON SCROLL
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%' //create animation when about halfway do the section
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%' //create animation when about halfway do the section
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%' //create animation when about halfway do the section
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%' //create animation when about halfway do the section
+    });
+    
+
 });
