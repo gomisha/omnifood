f132281c577d8ce2f82ec46ebde2931b6eeb07fc
gomisha <misha@gomisha.com>
2018-01-15 08:31:35 -0500

    added mobile nav icon and menu with jquery animation effects

--- resources/js/script.js
+++ resources/js/script.js
@@ -86,6 +86,23 @@ $(document).ready(function() {
     }, {
         offset: '50%' //create animation when about halfway do the section
     });
-    
 
+    
+    //MOBILE NAVIGATION
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if(icon.hasClass('ion-navicon-round')) {        /* turn mobile menu icon into close icon */
+            icon.removeClass('ion-navicon-round');  
+            icon.addClass('ion-close-round');
+           
+        } else {                                        /* turn mobile close menu icon back into regular mobile icon */
+            icon.removeClass('ion-close-round');  
+            icon.addClass('ion-navicon-round');
+        }
+    });
+    
 });
